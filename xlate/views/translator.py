f0360ae6d3946d607998803eaaa7a6fdda8adc20
Miguel Nistal <nistam328@gmail.com>
2019-12-08 14:03:31 -0500

    Filled in result page so we can look at stuff

--- xlate/views/translator.py
+++ xlate/views/translator.py
@@ -10,9 +10,11 @@ def index():
 @bp.route('/translate_textbox', methods=['POST'])
 def translate_textbox():
     print(request.form)
-    return render_template('index.html')
+    result = request.form.get('translation_text')
+    return render_template('result.html', result=result)
 
 @bp.route('/translate_file', methods=['POST'])
 def translate_file():
     print(request.form)
-    return render_template('index.html')
+    result = request.form.get('translation_file')
+    return render_template('index.html', result=result)
