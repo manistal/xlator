7f0e890a24150d7041413c7747638a4c4c90c9a9
Miguel A Nistal <nistam328@gmail.com>
2019-12-08 18:32:53 +0000

    Filled in the front-end structure and routes, hooked up the frontend to the backend routes

--- xlate/views/translator.py
+++ xlate/views/translator.py
@@ -7,3 +7,12 @@ bp = Blueprint('translator', __name__)
 def index():
     return render_template('index.html')
 
+@bp.route('/translate_textbox', methods=['POST'])
+def translate_textbox():
+    print(request.form)
+    return render_template('index.html')
+
+@bp.route('/translate_file', methods=['POST'])
+def translate_file():
+    print(request.form)
+    return render_template('index.html')
